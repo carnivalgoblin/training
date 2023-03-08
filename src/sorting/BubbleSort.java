43713a3ae9d19c1a37943b118815f68ae400f4ac
Rico Prodan <rico.prodan@gmail.com>
2023-03-08 16:27:06 +0100

    formatting

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,23 +1,26 @@
-package sorting;
-
-public class BubbleSort {
-
-  /**
-   * This method sorts an array of integers using the bubble sort algorithm.
-   * The input is an array of integers to be sorted, and the output is the sorted array.
-   * The bubble sort algorithm repeatedly steps through the list to be sorted,
-   * compares each pair of adjacent items and swaps them if they are in the wrong order.
-   * The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted.
-   *
-   * @param numbers The array of integers to be sorted
-   * @return The sorted array of integers
-   */
-
-  public int[] sort(int[] numbers) {
-
-    for (int i = 0; i  > 0; i++)
-
-    return numbers;
-  }
-
-}
+//package sorting;
+//
+//public class BubbleSort {
+//
+//  /**
+//   * This method sorts an array of integers using the bubble sort algorithm.
+//   * The input is an array of integers to be sorted, and the output is the sorted array.
+//   * The bubble sort algorithm repeatedly steps through the list to be sorted,
+//   * compares each pair of adjacent items and swaps them if they are in the wrong order.
+//   * The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted.
+//   *
+//   * @param numbers The array of integers to be sorted
+//   * @return The sorted array of integers
+//   */
+//
+//  public int[] sort(int[] numbers) {
+//
+//    for (int i = 0; i > 0; i++) {
+//
+//
+//      return numbers;
+//    }
+//  }
+//
+//}
+//
