ef5d02d17ff5800aa4e4875d44f56155f14c1f38
Rico Prodan <rico.prodan@gmail.com>
2023-03-08 13:27:34 +0100

    data types operations

---
+++ src/aufgabe1/Aufgabe1.java
@@ -0,0 +1,39 @@
+package aufgabe1;
+
+public class Aufgabe1 {
+
+
+  // a) 5
+  // int 5
+
+  // b) 5.0
+  // double 5.0
+
+  // c) 4L
+  // long 4
+
+  // d) 3 + 9
+  // int 12
+
+  // e) 2 + 6.0
+  // double 8.0
+
+  // f) 5 + 3.0f
+  // float 8.0
+
+  // g) 3d + 1.0f
+  // double 4.0
+
+  // h) 6L * 3
+  // long 18
+
+  // i) 7 / 2
+  // int 3
+
+  // j) 9 / 2.0
+  // double 4.5
+
+  // k) int i = 2; i--;
+  // int 1
+
+}
