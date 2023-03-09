d1c4146b3d94c9c9dc269b769055fb30aad9d4d2
Rico Prodan <rico.prodan@gmail.com>
2023-03-09 15:19:39 +0100

    kleinstes gemeinsames vielfaches

---
+++ src/aufgabe6/KleinstesGemeinsamesVielfaches.java
@@ -0,0 +1,46 @@
+package aufgabe6;
+
+import java.util.Scanner;
+
+public class KleinstesGemeinsamesVielfaches {
+
+  public static void main(String[] args) {
+
+    Scanner divisor1 = new Scanner(System.in);
+    System.out.println("Divisor 1:");
+    int div1 = Integer.parseInt(divisor1.nextLine());
+
+    Scanner divisor2 = new Scanner(System.in);
+    System.out.println("Divisor 2:");
+    int div2 = Integer.parseInt(divisor2.nextLine());
+
+    if (div1 <= 0 || div2 <= 0) {
+      System.out.println("Bitte nur positive, ganze Zahlen eingeben.");
+    } else {
+      System.out.println("Das kleinste gemeinsame Vielfache von " + div1 + " und " + div2 + " ist " + smallestDivisor(div1, div2));
+    }
+  }
+
+  public static int smallestDivisor(int div1, int div2) {
+
+    if (div1 > div2) {
+      int i = 1;
+      int rest = div2 % div1;
+      while (rest > 0) {
+        rest = (div2 * i) % div1;
+        i++;
+      }
+      return (i - 1) * div2;
+    } else {
+      int i = 1;
+      int rest = div1 % div2;
+      while (rest > 0) {
+        rest = (div1 * i) % div2;
+        i++;
+      }
+      return (i - 1) * div1;
+    }
+
+  }
+
+}
---
+++ test/aufgabe6/KleinstesGemeinsamesVielfachesTest.java
@@ -0,0 +1,21 @@
+package aufgabe6;
+
+import aufgabe5.BMICalculatorEnum;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KleinstesGemeinsamesVielfachesTest {
+
+  @Test
+  public void isSmallestCommonDivisor() {
+    int div1 = 3;
+    int div2 = 5;
+
+    assertTrue(KleinstesGemeinsamesVielfaches.smallestDivisor(div1, div2) == 15);
+    assertFalse(KleinstesGemeinsamesVielfaches.smallestDivisor(div1, div2) == 10);
+    assertFalse(KleinstesGemeinsamesVielfaches.smallestDivisor(-div1, div2) == -15);
+    assertFalse(KleinstesGemeinsamesVielfaches.smallestDivisor(-div1, div2) == 15);
+  }
+
+}
