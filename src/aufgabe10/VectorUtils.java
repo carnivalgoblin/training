e0f03acdbbf1f308593d1292f379c7dc2f11cf66
Rico Prodan <rico.prodan@gmail.com>
2023-03-13 14:28:39 +0100

    added comments for methods

--- src/aufgabe10/VectorUtils.java
+++ src/aufgabe10/VectorUtils.java
@@ -4,6 +4,12 @@ import static java.lang.Math.sqrt;
 
 public class VectorUtils {
 
+  /**
+   * Method to calculate dot product of two vectors
+   * @param v1 - Vector object with x, y and z values
+   * @param v2 - Vector object with x, y and z values
+   * @return - double with dot product of Vectors v1 and v2
+   */
   public static double dotProduct(Vector v1, Vector v2) {
 
     double tempX = v1.getX() * v2.getX();
@@ -13,6 +19,12 @@ public class VectorUtils {
     return (tempX + tempY + tempZ);
   }
 
+  /**
+   * Method to calculate dot division of two vectors
+   * @param v1 - Vector object with x, y and z values
+   * @param v2 - Vector object with x, y and z values
+   * @return - double with dot division of Vectors v1 and v2
+   */
   public static double dotProductDivision(Vector v1, Vector v2) {
     double tempX = v1.getX() / v2.getX();
     double tempY = v1.getY() / v2.getY();
@@ -21,6 +33,12 @@ public class VectorUtils {
     return (tempX + tempY + tempZ);
   }
 
+  /**
+   * Method to calculate cross product of two vectors
+   * @param v1 - Vector object with x, y and z values
+   * @param v2 - Vector object with x, y and z values
+   * @return - Vector object with cross product of v1 and v2
+   */
   public static Vector crossProduct(Vector v1, Vector v2) {
 
     double tempX = v1.getY() * v2.getZ() +  v1.getZ() * v2.getY();
@@ -30,6 +48,12 @@ public class VectorUtils {
     return new Vector(tempX, tempY, tempZ);
   }
 
+  /**
+   * Method to calculate addition of two vectors
+   * @param v1 - Vector object with x, y and z values
+   * @param v2 - Vector object with x, y and z values
+   * @return - Vector object with values of v1 and v2 added
+   */
   public static Vector addVector(Vector v1, Vector v2) {
     double tempX = v1.getX() +  v2.getX();
     double tempY = v1.getY() +  v2.getY();
@@ -37,7 +61,12 @@ public class VectorUtils {
 
     return new Vector(tempX, tempY, tempZ);
   }
-
+  /**
+   * Method to calculate subtraction of two vectors
+   * @param v1 - Vector object with x, y and z values
+   * @param v2 - Vector object with x, y and z values
+   * @return - Vector object with values of v1 and v2 subtracted
+   */
   public static Vector subtractVector(Vector v1, Vector v2) {
     double tempX = v1.getX() -  v2.getX();
     double tempY = v1.getY() -  v2.getY();
@@ -46,6 +75,11 @@ public class VectorUtils {
     return new Vector(tempX, tempY, tempZ);
   }
 
+  /**
+   * Method to calculate length of a vector
+   * @param v1 - Vector object with x, y and z values
+   * @return - double with the length of the Vector v1
+   */
   public static double lengthVector(Vector v1) {
     return Math.sqrt(v1.getX()* v1.getX() + v1.getY()* v1.getY() + v1.getZ()* v1.getZ());
   }
